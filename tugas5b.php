61dc4f1dda691fb94879d630306df7183feb81d7
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-10-08 10:36:50 +0700

    Add files via upload

---
+++ tugas5b.php
@@ -0,0 +1,25 @@
+<?php
+$a="Ayo";
+$b="Bersama";
+$c="Belajar";
+$d="Niomic";
+
+echo "<br><h2>Operator String</h2><br>";
+echo "<table border=1>
+        <tr>
+          <td>Input 1</td>
+          <td>Input 2</td>
+          <td>Hasil</td>
+        </tr>
+        <tr>
+          <td>",$a,"</td>
+          <td>".$c."</td>
+          <td>".$a." ".$c."</td>
+        </tr>
+        <tr>
+          <td>",$b,"</td>
+          <td>".$d."</td>
+          <td>".$b." ".$d."</td>
+        </tr>
+      </table>";
+?>
